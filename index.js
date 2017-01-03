89e7d11f4f7803bc0f488c801f0a6b85479f0fbd
Kameron R. Sween <kameron@krsween.com>
2017-01-03 13:11:58 -0600

    ES5'd everything for karma

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ Array.prototype.includes = Array.prototype.includes || function (searchElement, 
         throw new TypeError('Array.prototype.includes called on null or undefined');
     }
     if (fromIndex === undefined){
-        let i = this.length;
+        var i = this.length;
         while (i--) {
             if (this[i] === searchElement) {
                 return true;
             }
         }
     } else {
-        let i = fromIndex, len = this.length;
+        var i = fromIndex, len = this.length;
         while (i++ !== len) {
             if (this[i] === searchElement) {
                 return true;
