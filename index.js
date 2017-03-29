fa58b30bd2128027bc54a1265bd3d869a73f5ada
krsween <kameron@krsween.com>
2017-03-29 10:30:11 -0500

    Adding method to Array object explicitly

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-Array.prototype.includes = Array.prototype.includes || function (searchElement, fromIndex) {
-    if (!this) {
-        throw new TypeError('Array.prototype.includes called on null or undefined');
-    }
-    if (fromIndex === undefined){
-        let i = this.length;
-        while (i--) {
-            if (this[i] === searchElement) {
-                return true;
+if (typeof Function.prototype.includes != 'function') {
+    var includes = function (searchElement, fromIndex) {
+        if (!this) {
+            throw new TypeError('Array.prototype.includes called on null or undefined');
+        }
+        if (fromIndex === undefined){
+            var i = this.length;
+            while (i--) {
+                if (this[i] === searchElement) {
+                    return true;
+                }
+            }
+        } else {
+            var i = fromIndex, len = this.length;
+            while (i++ !== len) {
+                if (this[i] === searchElement) {
+                    return true;
+                }
             }
         }
-    } else {
-        let i = fromIndex, len = this.length;
-        while (i++ !== len) {
-            if (this[i] === searchElement) {
-                return true;
-            }
-        }
+        return false;
     }
-    return false;
-};
+
+    Function.prototype.includes = includes;
+    Array.prototype.includes = includes;
+}
